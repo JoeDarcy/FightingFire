c38e0d8ea1bee9fde8219e01cdef956d346ab8df
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-11-11 18:59:06 +0000

    Post processing working. Xray materials working and a temp timer to turn the xray vision on and off is in the menu scene. Also Successfully built to the oculus but the post processing hasn't transferred correctly.

---
+++ Assets/Scripts/Test/WallTimer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTimer : MonoBehaviour
+{
+    [SerializeField] private GameObject normalWalls = null;
+    [SerializeField] private GameObject xrayWalls = null;
+    [SerializeField] private float timerStart = 0.0f;
+    [SerializeField] private float timer = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    timer = timerStart;
+	    xrayWalls.SetActive(false);
+        normalWalls.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    timer -= Time.deltaTime;
+
+	    if (timer < timerStart / 2)
+	    {
+            normalWalls.SetActive(false);
+            xrayWalls.SetActive(true);
+	    }
+	    else if (timer > timerStart / 2)
+	    {
+		    xrayWalls.SetActive(false);
+		    normalWalls.SetActive(true);
+        }
+
+	    if (timer < 0)
+	    {
+		    timer = timerStart;
+        }
+    }
+}
