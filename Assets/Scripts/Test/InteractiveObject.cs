8eb7a48534404138d199ce1a080e88de3ba93e40
Joe34441 <69796715+Joe34441@users.noreply.github.com>
2021-12-02 19:48:02 +0000

    update before merge from main

--- Assets/Scripts/Test/InteractiveObject.cs
+++ Assets/Scripts/Test/InteractiveObject.cs
@@ -17,7 +17,7 @@ public class InteractiveObject : MonoBehaviour
     private string flammableTag = "Flammable";
     private bool isSpreading = false; //might not need
     private bool burntOut = false;
-    private List<GameObject> abc = new List<GameObject>();// = null;
+    private List<GameObject> collidedObjects = new List<GameObject>();// = null;
     private bool doOnce = true;
     private CapsuleCollider normalSpreadColliderHorizontalX = null;
     private CapsuleCollider normalSpreadColliderHorizontalZ = null;
@@ -52,8 +52,6 @@ public class InteractiveObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
         if (isBurning && canSpeadFire)
         {
             timerHorizontal += Time.deltaTime;
@@ -62,9 +60,9 @@ public class InteractiveObject : MonoBehaviour
             if (timerClose >= timeToCloseSpread)
             {
                 timerClose = 0.0f;
-                if (abc.Count > 0)
+                if (collidedObjects.Count > 0)
                 {
-                    foreach (GameObject obj in abc)
+                    foreach (GameObject obj in collidedObjects)
                     {
                         if (!obj.GetComponent<InteractiveObject>().isBurning)
                         {
@@ -129,23 +127,22 @@ public class InteractiveObject : MonoBehaviour
                                   // and vertical colliders will start the close spread collider
         //**************************************************************************************************************
         {
-            if (other.CompareTag(flammableTag))// && isBurning) //isBurning should be true...
+            if (isBurning) //ontriggerenter will only call at the start when it shouldnt spread here
             {
                 bool burning = other.gameObject.GetComponent<InteractiveObject>().isBurning;
                 bool hasBurntOut = other.gameObject.GetComponent<InteractiveObject>().burntOut;
-                if (abc.Count > 0)
+                if (collidedObjects.Count > 0)
                 {
-                    if (!abc.Contains(other.gameObject) && !burning && !hasBurntOut)
+                    if (!collidedObjects.Contains(other.gameObject) && !burning && !hasBurntOut)
                     {
-                        abc.Add(other.gameObject);
+                        collidedObjects.Add(other.gameObject);
                     }
                 }
                 else if (other.CompareTag(flammableTag))
                 {
-                    abc.Add(other.gameObject);
+                    collidedObjects.Add(other.gameObject);
                 }
             }
         }
-
     }
 }
