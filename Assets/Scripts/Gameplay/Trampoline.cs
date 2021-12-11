67a6fb8cc08c83ce36fab6c285cb20ff106eb07f
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-12-11 16:23:15 +0000

    Points text working.

--- Assets/Scripts/Gameplay/Trampoline.cs
+++ Assets/Scripts/Gameplay/Trampoline.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Trampoline : MonoBehaviour
 {
 	public static int points = 0;
+	public TextMeshPro pointsText = null;
 
 	private void Update() 
 	{
 		Debug.Log("Points: " + points);
+
+		pointsText.text = points.ToString();
 	}
 
 	private void OnTriggerEnter(Collider other) 
