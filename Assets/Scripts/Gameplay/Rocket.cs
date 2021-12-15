cedb53df266c055d49539f5c06fd546046ea8e28
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-12-15 11:20:23 +0000

    Rocket launcher and flame thrower setup visually. Both need interaction for grab and damage.

--- Assets/Scripts/Gameplay/Rocket.cs
+++ Assets/Scripts/Gameplay/Rocket.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void OnTriggerEnter(Collider other) 
+	{
+		Destroy(this);
+	}
 }
